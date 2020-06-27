e13425d0c994977084009b07f5b384ba699578d2
Shabbir Saifee <shabbirsaifee91@gmail.com>
2020-06-27 03:23:31 +0000

    add gem devise

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -61,4 +61,5 @@ Rails.application.configure do
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
   
   config.hosts << "8bc9cb5fec184d17899361cba016b425.vfs.cloud9.us-east-2.amazonaws.com"
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 end
